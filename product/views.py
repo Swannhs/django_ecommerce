347e8e8537980a18057833bb3c4bf64073332f42
Swannhs <princeapurbask@gmail.com>
2021-05-27 04:20:12 +0600

    Throw rest api from database

--- product/views.py
+++ product/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from product.models import Product
+from product.serializers import ProductSerializer
+
+
+@api_view(['GET'])
+def getProducts(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
