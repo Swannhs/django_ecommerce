bfdfb86bf3eb5f256e2e8da2b6b8c73c37e4abaf
Swannhs <princeapurbask@gmail.com>
2021-05-27 16:49:12 +0600

    Added new model for order & order item

---
+++ order/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+from product.models import Product
+
+
+class Order(models.Model):
+    id = models.AutoField(primary_key=True, editable=False)
+    paymentMethod = models.CharField(max_length=200, null=True, blank=True)
+    taxPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    shippingPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    totalPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    isPaid = models.BooleanField(default=False)
+    paidAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    isDelivered = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.createdAt)
+
+
+class OrderItem(models.Model):
+    id = models.AutoField(primary_key=True, editabel=False)
+    # customer name
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.ImageField(null=True, blank=True, default=0)
+
+    def __str__(self):
+        return str(self.product)
