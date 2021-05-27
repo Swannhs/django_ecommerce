f3c899cace9d3c51d4c7256a7d3bc3fcd02073fc
Swannhs <princeapurbask@gmail.com>
2021-05-27 17:38:30 +0600

    Created view for response order

--- order/views.py
+++ order/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from order.models import Order
+from order.serializers import OrderSerializer
+
+
+@api_view(['GET'])
+def getOrders(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
