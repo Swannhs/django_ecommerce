cdf0b7733684e0c4b0af16287848688c8bd8f1b3
Swannhs <princeapurbask@gmail.com>
2021-05-27 23:47:25 +0600

    Created model Customer with basic info and shipping address

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+from order.models import OrderItem
+
+
+class BasicInfo(models.Model):
+    id = models.AutoField(primary_key=True, editable=False)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    fullName = models.CharField(max_length=200, null=True, blank=True)
+    address = models.CharField(max_length=200, null=True, blank=True)
+    city = models.CharField(max_length=200, null=True, blank=True)
+    postalCode = models.CharField(max_length=200, null=True, blank=True)
+    mobile = models.CharField(max_length=20, null=True, blank=True)
+
+
+class ShippingAddress(models.Model):
+    id = models.AutoField(primary_key=True, editable=False)
+    order = models.OneToOneField(OrderItem, on_delete=models.CASCADE, null=True, blank=True)
+    address = models.CharField(max_length=200, null=True, blank=True)
+    postalCode = models.CharField(max_length=200, null=True, blank=True)
+    shippingPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.address)
