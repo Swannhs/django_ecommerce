edb4806e3886d8966e8037d68035a55db696d513
Swannhs <princeapurbask@gmail.com>
2021-05-27 23:52:13 +0600

    Configured order item data for view

--- order/views.py
+++ order/views.py
@@ -4,12 +4,19 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from order.models import Order
-from order.serializers import OrderSerializer
+from order.models import Order, OrderItem
+from order.serializers import OrderSerializer, OrderItemSerializer
 
 
 @api_view(['GET'])
 def getOrders(request):
     orders = Order.objects.all()
     serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getOrderItems(request):
+    items = OrderItem.objects.all()
+    serializer = OrderItemSerializer(items, many=True)
     return Response(serializer.data)
