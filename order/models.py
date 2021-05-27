c4183d65383e08c77659c2b01d703c882e93d92d
Swannhs <princeapurbask@gmail.com>
2021-05-27 23:47:52 +0600

    No heavy changed

--- order/models.py
+++ order/models.py
@@ -8,7 +8,6 @@ class Order(models.Model):
     id = models.AutoField(primary_key=True, editable=False)
     paymentMethod = models.CharField(max_length=200, null=True, blank=True)
     taxPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
-    shippingPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     totalPrice = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
     isPaid = models.BooleanField(default=False)
     paidAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
