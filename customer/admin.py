20b4f37f53316dd7151ba16f92ff485803a72d35
Swannhs <princeapurbask@gmail.com>
2021-05-27 23:46:25 +0600

    Register shipping address & basic info

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from customer.models import ShippingAddress, BasicInfo
+
+# admin.site.register(Info)
+admin.site.register(ShippingAddress)
+admin.site.register(BasicInfo)
