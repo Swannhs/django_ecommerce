34b05a972bb12805dd016c2af92e7096834662e6
Swannhs <princeapurbask@gmail.com>
2021-05-27 23:51:39 +0600

    Serialized customers data

--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from customer.models import BasicInfo
+from customer.serializer import CustomerInfoSerializer
+
+
+@api_view(['GET'])
+def getCustomers(request):
+    customers = BasicInfo.objects.all()
+    serializer = CustomerInfoSerializer(customers)
+    return Response(serializer.data)
